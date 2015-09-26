2051dd79688f3af24f2b0140cf65a6fc3ccd64b2
Berengere <berengere.ma@gmail.com>
2015-09-26 14:01:47 +0200

    correct bug in parser

--- src/core/Agent.java
+++ src/core/Agent.java
@@ -5,207 +5,274 @@ import processing.core.PVector;
 
 
 public class Agent{
-  PApplet parent;
-  PVector pos;//cordinate of agent's center of mass
-  PVector speed;//agent's speed
-  String name;//agent's name
-  boolean looping;
-  
-  boolean radarActive;
-  PVector radarState;
-  
-  //default constructor  
-  public Agent(PApplet parent, float x,float y){
-	  this.parent = parent;
-	  this.name = "";
-	  this.looping = false;
-	  this.radarActive = false;
-	  pos=new PVector(x,y);//initialize position
-	  speed=new PVector(0,0);//no speed
-	  radarState=new PVector(50, 50); // radar starts in the border of Agent
-  }
-  
-  public boolean isLooping() {
-	return looping;
-  }
-  
-  public boolean isMoving(){
-      return speed.mag()>=1;
-  }
-  
-  public void setLooping(boolean looping) {
-	this.looping = looping;
-  }
-  
-  
-  public void setRadarActive(boolean radarActive){
-	  this.radarActive = radarActive;
-  }
-  
-  public boolean isRadarActive() {
-	return radarActive;
-  }
-  
-  public boolean radarReadsRectangle(PShape obstacles){
-	  
-	  float[] obstacleParameters;
-	  PVector[] corners = new PVector[4];
-	  for (int i=0;i<4;i++)
-		  corners[i] = new PVector(0, 0);
-	  
-	  float[] obstacle_limits = new float[4];
-	  float[] farthest_lines = new float[4];
-	  
-	  float x, y;
-	  float dist;
+	PApplet parent;
+	PVector pos;//cordinate of agent's center of mass
+	PVector speed;//agent's speed
+	String name;//agent's name
+	boolean looping;
 
-	  if(radarActive){
-		  for(int i=0; i < obstacles.getChildCount(); i++){
-			   obstacleParameters = obstacles.getChild(i).getParams();
-			   //left
-			   obstacle_limits[0] = obstacleParameters[0] - .5f*obstacleParameters[2];
-			   //right
-			   obstacle_limits[1] = obstacleParameters[0] + .5f*obstacleParameters[2];
-			   //upper
-			   obstacle_limits[2] = obstacleParameters[1] - .5f*obstacleParameters[3];
-			   //lower
-			   obstacle_limits[3] = obstacleParameters[1] + .5f*obstacleParameters[3];
-			   
-			   // Test Center
-			   if(pos.x >= obstacle_limits[0] && pos.x <= obstacle_limits[1])
-				   if(pos.y >= obstacle_limits[2] && pos.y <= obstacle_limits[3])
-					   return true;
+	boolean radarActive;
+	PVector radarState;
 
-			   
-			   // The center is not colliding, test corners
-			   for(int j=0; j<2; j++){
-				   x = obstacle_limits[j];
-				   for(int k=2; k<4; k++){
-				       y = obstacle_limits[k];
-				       dist= (float)Math.sqrt(
-				    		   Math.pow((x-pos.x), 2) + 
-				    		   Math.pow((y-pos.y), 2)
-				    		   );
-				       if(dist <= radarState.x/2.0f)
-				    	   return true;
-				   }
-				   
-			   }
+	//default constructor  
+	public Agent(PApplet parent, float x,float y){
+		this.parent = parent;
+		this.name = "";
+		this.looping = false;
+		this.radarActive = false;
+		pos=new PVector(x,y);//initialize position
+		speed=new PVector(0,0);//no speed
+		radarState=new PVector(50, 50); // radar starts in the border of Agent
+	}
 
-			   // The corners are not colliding, test limits
-			   //left of Agent
-			   farthest_lines[0] = pos.x - .5f*radarState.x;
-			   //right of Agent
-			   farthest_lines[1] = pos.x + .5f*radarState.x;
-			   //up of Agent
-			   farthest_lines[2] = pos.y - .5f*radarState.y;
-			   //down of Agent
-			   farthest_lines[3] = pos.y + .5f*radarState.y;
-			   for(int j =0; j<4; j++)
-			   {
-				   if (farthest_lines[j] >= obstacle_limits[0] && farthest_lines[j] <= obstacle_limits[1])
-					   if(farthest_lines[j] >= obstacle_limits[2] && farthest_lines[j] <= obstacle_limits[3])
-						   return true;
-			   }
-		  }
-	  }
-	  return false;
-  }
-  
-  //speed up
-  public void speed_up(){
-	  if(speed.mag()==0){
-          speed.set(speed.x+10,speed.y);
-	      return;
-	  }
-	  if(speed.x>.9)
-	      speed.set(speed.x+10, speed.y);
-	  else if(speed.x<-.9)
-		  speed.set(speed.x-10, speed.y);
-	  if(speed.y>.9)
-		  speed.set(speed.x, speed.y+10);
-	  else if(speed.y<-.9)
-		  speed.set(speed.x, speed.y-10);
-  }
-  
-  public void setName(String name) {
-	this.name = name;
-  }
-  
-  public String getName() {
-	return name;
-  }
-  
-  public void stop(){
-	    speed.set(0.0f, 0.0f);
-  }
-  
-  public void turnRight(){
-	  if(speed.mag() > 0){
-		speed.rotate((float)(.5f * Math.PI));
-	  }
-	  System.out.println(speed);
-  }
-  
-  public void turnLeft(){
-      if(speed.mag() > 0){
-    	  speed.rotate((float)(-.5f * Math.PI));  
-	  }
-  }
-  
-  public void setPos(float x, float y){
-	  pos.set(x, y);
-  }
-  
-  public PVector getPos() {
-	return pos;
-  }
-  
-  //update agent position
-  public void update(){
-    pos.add(speed);
-    if(looping && speed.mag()>0){
-    	if(isOutOfBounds()){
-    		if(speed.x > 0 && pos.x > parent.width){
-    			setPos(-50, pos.y);	
-    		}else if(speed.x < 0 && pos.x < 0){
-    			setPos(parent.width-50, pos.y);
-    		}
-    		if (speed.y > 0 && pos.y > parent.height) {
-    			setPos(pos.x, -50);
-			}else if(speed.y < 0 && pos.y < 0 ){
-				setPos(pos.x, parent.height-50);
+	public boolean isLooping() {
+		return looping;
+	}
+
+	public boolean isMoving(){
+		return speed.mag()>=1;
+	}
+
+	public void setLooping(boolean looping) {
+		this.looping = looping;
+	}
+
+
+	public void setRadarActive(boolean radarActive){
+		this.radarActive = radarActive;
+	}
+
+	public boolean isRadarActive() {
+		return radarActive;
+	}
+
+	public boolean radarReadsRectangle(PShape obstacles){
+
+		float[] obstacleParameters;
+		PVector[] corners = new PVector[4];
+		for (int i=0;i<4;i++)
+			corners[i] = new PVector(0, 0);
+
+		float[] obstacle_limits = new float[4];
+		float[] farthest_lines = new float[4];
+
+		float x, y;
+		float dist;
+
+		if(radarActive){
+			for(int i=0; i < obstacles.getChildCount(); i++){
+				obstacleParameters = obstacles.getChild(i).getParams();
+				//left
+				obstacle_limits[0] = obstacleParameters[0] - .5f*obstacleParameters[2];
+				//right
+				obstacle_limits[1] = obstacleParameters[0] + .5f*obstacleParameters[2];
+				//upper
+				obstacle_limits[2] = obstacleParameters[1] - .5f*obstacleParameters[3];
+				//lower
+				obstacle_limits[3] = obstacleParameters[1] + .5f*obstacleParameters[3];
+
+				// Test Center
+				if(pos.x >= obstacle_limits[0] && pos.x <= obstacle_limits[1])
+					if(pos.y >= obstacle_limits[2] && pos.y <= obstacle_limits[3])
+						return true;
+
+
+				// The center is not colliding, test corners
+				for(int j=0; j<2; j++){
+					x = obstacle_limits[j];
+					for(int k=2; k<4; k++){
+						y = obstacle_limits[k];
+						dist= (float)Math.sqrt(
+								Math.pow((x-pos.x), 2) + 
+								Math.pow((y-pos.y), 2)
+								);
+						if(dist <= radarState.x/2.0f)
+							return true;
+					}
+
+				}
+
+				// The corners are not colliding, test limits
+				//left of Agent
+				farthest_lines[0] = pos.x - .5f*radarState.x;
+				//right of Agent
+				farthest_lines[1] = pos.x + .5f*radarState.x;
+				//up of Agent
+				farthest_lines[2] = pos.y - .5f*radarState.y;
+				//down of Agent
+				farthest_lines[3] = pos.y + .5f*radarState.y;
+				for(int j =0; j<4; j++)
+				{
+					if (farthest_lines[j] >= obstacle_limits[0] && farthest_lines[j] <= obstacle_limits[1])
+						if(farthest_lines[j] >= obstacle_limits[2] && farthest_lines[j] <= obstacle_limits[3])
+							return true;
+				}
 			}
-    	}
-    }
-    if (radarActive){
-  		parent.stroke(255);
-  		parent.noFill();
-    	parent.ellipse(pos.x, pos.y, radarState.x, radarState.y);
-    	radarState.add(new PVector(3,  3));
-    	if(radarState.x > 175)
-    	    radarState = new PVector(50, 50);
-    }
-  }
-  
-  public boolean isOutOfBounds(){
-	  if(pos.x > parent.width || pos.x < 0 || pos.y > parent.height || pos.y < 0)
-		  return true;
-	  else
-		  return false;
-  }
-  
-  //draw agent
-  //because I have no gift for drawing
-  //agent is only a white disk
-  public void draw(){
-	  //Erase previous position
-	parent.fill(0);
-	parent.noStroke();
-	parent.ellipse(pos.x, pos.y, 51, 51);
-    update();
-    parent.fill(255);
-    parent.noStroke();
-    parent.ellipse(pos.x, pos.y, 50, 50);
-  }
+		}
+		return false;
+	}
+
+	private boolean touch_obstacles(PShape obstacles){
+		float[] obstacleParameters;
+		PVector[] corners = new PVector[4];
+		for (int i=0;i<4;i++)
+			corners[i] = new PVector(0, 0);
+
+		float[] obstacle_limits = new float[4];
+		float[] farthest_lines = new float[4];
+
+		float x, y;
+		float dist;
+
+		for(int i=0; i < obstacles.getChildCount(); i++){
+			obstacleParameters = obstacles.getChild(i).getParams();
+			//left
+			obstacle_limits[0] = obstacleParameters[0] - .5f*obstacleParameters[2];
+			//right
+			obstacle_limits[1] = obstacleParameters[0] + .5f*obstacleParameters[2];
+			//upper
+			obstacle_limits[2] = obstacleParameters[1] - .5f*obstacleParameters[3];
+			//lower
+			obstacle_limits[3] = obstacleParameters[1] + .5f*obstacleParameters[3];
+
+			// Test Center
+			if(pos.x >= obstacle_limits[0] && pos.x <= obstacle_limits[1])
+				if(pos.y >= obstacle_limits[2] && pos.y <= obstacle_limits[3])
+					return true;
+
+
+			// The center is not colliding, test corners
+			for(int j=0; j<2; j++){
+				x = obstacle_limits[j];
+				for(int k=2; k<4; k++){
+					y = obstacle_limits[k];
+					dist= (float)Math.sqrt(
+							Math.pow((x-pos.x), 2) + 
+							Math.pow((y-pos.y), 2)
+							);
+					if(dist <= radarState.x/2.0f)
+						return true;
+				}
+
+			}
+
+			// The corners are not colliding, test limits
+			//left of Agent
+			farthest_lines[0] = pos.x - .5f*radarState.x;
+			//right of Agent
+			farthest_lines[1] = pos.x + .5f*radarState.x;
+			//up of Agent
+			farthest_lines[2] = pos.y - .5f*radarState.y;
+			//down of Agent
+			farthest_lines[3] = pos.y + .5f*radarState.y;
+			for(int j =0; j<4; j++)
+			{
+				if (farthest_lines[j] >= obstacle_limits[0] && farthest_lines[j] <= obstacle_limits[1])
+					if(farthest_lines[j] >= obstacle_limits[2] && farthest_lines[j] <= obstacle_limits[3])
+						return true;
+			}
+		}
+
+		return false;
+	}
+
+	//speed up
+	public void speed_up(){
+		if(speed.mag()==0){
+			speed.set(speed.x+10,speed.y);
+			return;
+		}
+		if(speed.x>.9)
+			speed.set(speed.x+10, speed.y);
+		else if(speed.x<-.9)
+			speed.set(speed.x-10, speed.y);
+		if(speed.y>.9)
+			speed.set(speed.x, speed.y+10);
+		else if(speed.y<-.9)
+			speed.set(speed.x, speed.y-10);
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void stop(){
+		speed.set(0.0f, 0.0f);
+	}
+
+	public void turnRight(){
+		if(speed.mag() > 0){
+			speed.rotate((float)(.5f * Math.PI));
+		}
+		System.out.println(speed);
+	}
+
+	public void turnLeft(){
+		if(speed.mag() > 0){
+			speed.rotate((float)(-.5f * Math.PI));  
+		}
+	}
+
+	public void setPos(float x, float y){
+		pos.set(x, y);
+	}
+
+	public PVector getPos() {
+		return pos;
+	}
+	
+	
+
+	//update agent position
+	public void update(){
+		pos.add(speed);
+		if(looping && speed.mag()>0){
+			if(isOutOfBounds()){
+				if(speed.x > 0 && pos.x > parent.width){
+					setPos(-50, pos.y);	
+				}else if(speed.x < 0 && pos.x < 0){
+					setPos(parent.width-50, pos.y);
+				}
+				if (speed.y > 0 && pos.y > parent.height) {
+					setPos(pos.x, -50);
+				}else if(speed.y < 0 && pos.y < 0 ){
+					setPos(pos.x, parent.height-50);
+				}
+			}
+			
+		}
+		if (radarActive){
+			parent.stroke(255);
+			parent.noFill();
+			parent.ellipse(pos.x, pos.y, radarState.x, radarState.y);
+			radarState.add(new PVector(3,  3));
+			if(radarState.x > 175)
+				radarState = new PVector(50, 50);
+		}
+	}
+
+	public boolean isOutOfBounds(){
+		if(pos.x > parent.width || pos.x < 0 || pos.y > parent.height || pos.y < 0)
+			return true;
+		else
+			return false;
+	}
+
+	//draw agent
+	//because I have no gift for drawing
+	//agent is only a white disk
+	public void draw(){
+		//Erase previous position
+		parent.fill(0);
+		parent.noStroke();
+		parent.ellipse(pos.x, pos.y, 51, 51);
+		update();
+		parent.fill(255);
+		parent.noStroke();
+		parent.ellipse(pos.x, pos.y, 50, 50);
+	}
 }
--- src/levels/Level2.java
+++ src/levels/Level2.java
@@ -6,7 +6,7 @@ import processing.core.PShape;
 import processing.core.PVector;
 
 public class Level2 extends Level{
-	
+
 	PShape obstacles;
 	PVector bg_color, color;
 	int state;
@@ -17,7 +17,7 @@ public class Level2 extends Level{
 		bg_color = new PVector(0, 0, 0);
 		color = new PVector(0, 255, 0);
 		state = 0;
-		
+
 		PShape upper_rect = parent.createShape(parent.RECT,
 				(float) (parent.width*0.75),
 				(float) 0, 
@@ -26,7 +26,7 @@ public class Level2 extends Level{
 				);
 		upper_rect.setFill(255);
 
-		
+
 		PShape lower_rect = parent.createShape(parent.RECT,
 				(float) (parent.width*0.75), 
 				(float) (parent.height*0.5+50), 
@@ -35,83 +35,83 @@ public class Level2 extends Level{
 				);
 		lower_rect.setFill(255);
 
-		
+
 		obstacles.addChild(upper_rect);
 		obstacles.addChild(lower_rect);
 	}
-	
+
 	public void drawScenery(){
 		parent.noStroke();
 		parent.shape(obstacles);
 	}
-	
+
 	public PShape getObstacles() {
 		return obstacles;
 	}
-	
+
 	@Override
 	public void draw() {
-	    
+
 		if(agent.radarReadsRectangle(obstacles))
 			bg_color=new PVector(255, 0, 0);
-		
+
 		parent.background(
 				bg_color.x,
 				bg_color.y,
 				bg_color.z);
-		
+
 		if (bg_color.mag()>0)
 			bg_color.sub(17, 0, 0);
 		else
 			bg_color.set(0, 0, 0);
 
 		drawScenery();
-		
-	      if(parser.isEnterTouch()){
-	            if(parser.executeInput(agent)){
-	                if(agent.isRadarActive() && state == 1)
-	                    state++;
-	                else if(!agent.isRadarActive() && state == 2){
-	                    color.set(255, 0, 0);
-	                    state++;
-	                }
-	                else if(!agent.isMoving() && state == 3){
-	                       color.set(0, 255, 0);
-	                    state++;
-	                }
-	                else if(agent.isRadarActive() && agent.isMoving() && state == 4){
-	                    color.set(0, 125, 255);
-	                    state++;
-	                    agent.setLooping(false);
-	                }
-	            }
-	            parser.clear();
-	        }
-		
+
+		if(parser.isEnterTouch()){
+			if(parser.executeInput(agent)){
+				if(agent.isRadarActive() && state == 1)
+					state++;
+				else if(!agent.isRadarActive() && state == 2){
+					color.set(255, 0, 0);
+					state++;
+				}
+				else if(!agent.isMoving() && state == 3){
+					color.set(0, 255, 0);
+					state++;
+				}
+				else if(agent.isRadarActive() && agent.isMoving() && state == 4){
+					color.set(0, 125, 255);
+					state++;
+					agent.setLooping(false);
+				}
+			}
+			parser.clear();
+		}
+
 		switch(state){
 		case 0:
-            /*agent comes from the left*/
-            agent.setPos(-50, agent.getPos().y);
-		    agent.setLooping(true);
-		    state++;
+			/*agent comes from the left*/
+			agent.setPos(-50, agent.getPos().y);
+			agent.setLooping(true);
+			state++;
 		case 1:
 			print_hint(color, "allumer.radar");
 			break;
 		case 2:
 			print_hint(color, "eteindre.radar");
 			break;
-	    case 3:
-            print_hint(color, "arreter");
-            break;
+		case 3:
+			print_hint(color, "arreter");
+			break;
 		case 4:
 			print_hint(color, "allumer.radar et accelerer");
 			break;
 		case 5:
-		    // You can type "continuer" or wait
-            if(parser.isLevelFinished() || agent.isOutOfBounds()){
-                parser.resetLevelFinished();
-                this.setFinished(true);
-            }
+			// You can type "continuer" or wait
+			if(parser.isLevelFinished() || agent.isOutOfBounds()){
+				parser.resetLevelFinished();
+				this.setFinished(true);
+			}
 			break;
 		}
 		//parent.textSize(32);
@@ -119,7 +119,7 @@ public class Level2 extends Level{
 		print_request();
 		agent.draw();
 	}
-	
+
 	@Override
 	public ILevel getNextLevel() {
 		// TODO Auto-generated method stub
